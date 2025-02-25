14d534fdd3f1a47892073adaa33eacf31d9e4998
Prathik Saduneni <151557515+prthik@users.noreply.github.com>
2025-02-25 16:51:20 -0500

    fixed about page error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,11 @@
 import About from "@/components/About";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 import React from "react";
 
 
 const AboutPage: React.FC = () => {
     return (
         <div>
-            <Navbar />
-            <About />
-            <Footer />
+        <About />
         </div>
     );
 };
